13b1a506f73e4cf1d6c8e953ff358a009279d710
The Holy Ghost <holyghost@ip-172-31-85-48.ec2.internal>
2020-07-29 17:49:49 +0000

    bugfix xdraw

--- xdraw.scm
+++ xdraw.scm
@@ -3,7 +3,7 @@
 (define (make-xdraw)
 	(let ((*fill-rectangle-functor (make-functor 
 			;; fill-rectangle from scx might also be used
-				(lambda (dpy win gc x y w h)
+				(lambda (args) ;; dpy win gc x y w h
 					(let ((points (* w h)))
 						(do ((xx x (+ xx 1)))
 						((= xx points) #t)
@@ -18,7 +18,7 @@
 		(set! *draw-functor f))
 
 	(define (draw dpy win gc x y  w  h)
-		((*draw-functor 'do) (list dpy win gc x y w h)))
+		((*draw-functor 'do) dpy win gc x y w h))
 		
 
 	(define (get-x-fill-rectangle-functor)
@@ -28,7 +28,7 @@
 		(set! *fill-rectangle-functor f))
 
 	(define (fill-rectangle2 dpy win gc x y  w  h) ;; NOTE : scx contains fill-rectangle
-		((*fill-rectangle-functor 'do) (list dpy win gc x y w h)))
+		((*fill-rectangle-functor 'do) dpy win gc x y w h))
 		
 
 	(define (dispatch msg)
